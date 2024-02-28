068a3481101f4d0f627a00f67aa77b61b7095c48
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-27 22:50:14 -0300

    Serializa api e endpoints

---
+++ src/main/java/br/com/fiap/financaspro/controller/CategoriaController.java
@@ -0,0 +1,28 @@
+package br.com.fiap.financaspro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CategoriaController {
+    @RequestMapping(method = RequestMethod.GET, path = "/categorias", produces = "application/json")
+    @ResponseBody
+    public String index() {
+        return """
+                [
+                    {
+                    "id": 1,
+                    "nome": "Alimentação"
+                    "icone": "fast-food"
+                }
+                {
+                    "id": 2,
+                    "nome": "educação"
+                    "icone": "book"
+                }
+                ]
+                """;
+    }
+}
